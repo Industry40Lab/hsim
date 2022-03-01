60793ff0d9721edc5de5737b2c6b3db1b74212c8
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-03-01 14:51:21 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hsim',
+      version='0.1',
+      description='%',
+      url='http://github.com/lorenzo-ragazzini/hsim',
+      author='lorenzo-ragazzini',
+      packages=['hsim'],
+      zip_safe=False)
