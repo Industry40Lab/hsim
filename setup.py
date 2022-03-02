88b8f9f3346a6ee778dafeb7aad02ff59f7f979b
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-03-02 09:38:54 +0100

    Move

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name='hsim',
     version='0.0.3',
     author='Lorenzo',
     author_email='_',
     description='_',
-    long_description=long_description,
+    long_description='0',
     long_description_content_type="text/markdown",
     url='https://github.com/lorenzo-ragazzini/hsim',
     license='MIT',
-    packages=['hsim'],
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
     # install_requires=['requests'],
 )
