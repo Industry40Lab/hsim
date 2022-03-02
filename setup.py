0151bcdc0b5330a21d36aa0c6166a0cfd70f017a
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-03-02 10:00:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hfsm",
+    name="hsim",
     version="0.0.1",
     author="Lorenzo",
     author_email="@",
