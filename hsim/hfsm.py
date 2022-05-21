579ac41276148a60d56569d8466813087da4d353
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-05-21 12:44:52 +0200

    Update hfsm.py

--- hsim/hfsm.py
+++ hsim/hfsm.py
@@ -7,318 +7,18 @@ Created on Sat Mar 19 16:15:38 2022
 
 from typing import List, Any, Optional, Callable
 import logging 
-
-'''
-class State(object):
-
-    def __init__(self, name, child_sm=None):
-        self._name = name
-        self._entry_callbacks: List[Callable[[Any], None]] = []
-        self._exit_callbacks: List[Callable[[Any], None]] = []
-        self._child_state_machine: Optional[StateMachine] = child_sm
-        self._parent_state_machine: Optional[StateMachine] = None
-
-    def __repr__(self):
-        return f"State({self.name})"
-
-    # def __eq__(self, other):
-    #     if other.name == self._name:
-    #         return True
-    #     else:
-    #         return False
-
-    # def __ne__(self, other):
-    #     return not self.__eq__(other)
-
-    def __call__(self, data: Any):
-        pass
-
-    def on_entry(self, callback: Callable[[Any], None]):
-        self._entry_callbacks.append(callback)
-
-    def on_exit(self, callback: Callable[[], None]):
-        self._exit_callbacks.append(callback)
-
-    def set_child_sm(self, child_sm):
-        if not isinstance(child_sm, StateMachine):
-            raise TypeError("child_sm must be the type of StateMachine")
-        if self._parent_state_machine and self._parent_state_machine == child_sm:
-            raise ValueError("child_sm and parent_sm must be different")
-        self._child_state_machine = child_sm
-
-    def set_parent_sm(self, parent_sm):
-        if not isinstance(parent_sm, StateMachine):
-            raise TypeError("parent_sm must be the type of StateMachine")
-        if self._child_state_machine and self._child_state_machine == parent_sm:
-            raise ValueError("child_sm and parent_sm must be different")
-        self._parent_state_machine = parent_sm
-        self.env = parent_sm.env
-
-    def start(self, data: Any):
-        logging.debug(f"Entering {self._name}")
-        for callback in self._entry_callbacks:
-            callback(data)
-        if self._child_state_machine is not None:
-            self._child_state_machine.start(data)
-
-    def stop(self, data: Any):
-        logging.debug(f"Exiting {self._name}")
-        for callback in self._exit_callbacks:
-            callback(data)
-        if self._child_state_machine is not None:
-            self._child_state_machine.stop(data)
-
-    def has_child_sm(self) -> bool:
-        return True if self._child_state_machine else False
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def child_sm(self):
-        return self._child_state_machine
-
-    @property
-    def parent_sm(self):
-        return self._parent_state_machine
-
-
-class ExitState(State):
-
-    def __init__(self, status="Normal"):
-        self._name = "ExitState"
-        self._status = status
-        super().__init__(self._status + self._name)
-
-    @property
-    def status(self):
-        return self._status
-
-
-class Event(object):
-
-    def __init__(self, name):
-        self._name = name
-
-    def __repr__(self):
-        return f"Event={self._name}"
-
-    def __eq__(self, other):
-        if other.name == self._name:
-            return True
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    @property
-    def name(self):
-        return self._name
-
-
-class Transition(object):
-
-    def __init__(self, event: Event, src: State, dst: State):
-        self._event = event
-        self._source_state = src
-        self._destination_state = dst
-        self._condition: Optional[Callable[[Any], bool]] = None
-        self._action: Optional[Callable[[Any], None]] = None
-
-    def __call__(self, data: Any):
-        raise NotImplementedError
-
-    def add_condition(self, callback: Callable[[Any], bool]):
-        self._condition = callback
-
-    def add_action(self, callback: Callable[[Any], Any]):
-        self._action = callback
-
-    @property
-    def event(self):
-        return self._event
-
-    @property
-    def source_state(self):
-        return self._source_state
-
-    @property
-    def destination_state(self):
-        return self._destination_state
-
-
-class NormalTransition(Transition):
-
-    def __init__(self, source_state: State, destination_state: State,
-                 event: Event):
-        super().__init__(event, source_state, destination_state)
-        self._from = source_state
-        self._to = destination_state
-
-    def __call__(self, data: Any):
-        if not self._condition or self._condition(data):
-            logging.info(f"NormalTransition from {self._from} to {self._to} "
-                         f"caused by {self._event}")
-            if self._action:
-                self._action(data)
-            self._from.stop(data)
-            self._to.start(data)
-
-    def __repr__(self):
-        return f"Transition {self._from} to {self._to} by {self._event}"
-
-
-class SelfTransition(Transition):
-
-    def __init__(self, source_state: State, event: Event):
-        super().__init__(event, source_state, source_state)
-        self._state = source_state
-
-    def __call__(self, data: Any):
-        if not self._condition or self._condition(data):
-            logging.info(f"SelfTransition {self._state}")
-            if self._action:
-                self._action(data)
-            self._state.stop(data)
-            self._state.start(data)
-
-    def __repr__(self):
-        return f"SelfTransition on {self._state}"
-
-
-class NullTransition(Transition):
-
-    def __init__(self, source_state: State, event: Event):
-        super().__init__(event, source_state, source_state)
-        self._state = source_state
-
-    def __call__(self, data: Any):
-        if not self._condition or self._condition(data):
-            logging.info(f"NullTransition {self._state}")
-            if self._action:
-                self._action(data)
-
-    def __repr__(self):
-        return f"NullTransition on {self._state}"
-
-
-class StateMachine(object):
-
-    def __init__(self, name):
-        self._name = name
-        self._states: List[State] = []
-        self._transitions: List[Transition] = []
-        self._initial_state: Optional[List[State]] = None
-        self._current_state: Optional[List[State]] = None
-        # self._exit_callback: Optional[Callable[[ExitState, Any], None]] = None
-        # self._exit_state = ExitState()
-        # self.add_state(self._exit_state)
-        # self._exited = True
-        
-    def __eq__(self, other):
-        if other.name == self._name:
-            return True
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __str__(self):
-        return self._name
-
-
-
-    def on_exit(self, callback):
-        self._exit_callback = callback
-
-
-
-    def add_state(self, state: State, initial_state: bool = False):
-        if state in self._states:
-            raise ValueError("attempting to add same state twice")
-        self._states.append(state)
-        state.set_parent_sm(self)
-        if not self._initial_state and initial_state:
-            self._initial_state = state
-
-    def add_event(self, event: Event):
-        self._events.append(event)
-
-    def add_transition(self, src: State, dst: State, evt: Event) -> \
-            Optional[Transition]:
-        transition = None
-        if src in self._states and dst in self._states and evt in self._events:
-            transition = NormalTransition(src, dst, evt)
-            self._transitions.append(transition)
-        return transition
-
-    def add_self_transition(self, state: State, evt: Event) -> \
-            Optional[Transition]:
-        transition = None
-        if state in self._states and evt in self._events:
-            transition = SelfTransition(state, evt)
-            self._transitions.append(transition)
-        return transition
-
-    def add_null_transition(self, state: State, evt: Event) -> \
-            Optional[Transition]:
-        transition = None
-        if state in self._states and evt in self._events:
-            transition = NullTransition(state, evt)
-            self._transitions.append(transition)
-        return transition
-
-    def trigger_event(self, evt: Event, data: Any = None,
-                      propagate: bool = False):
-        transition_valid = False
-        if not self._initial_state:
-            raise ValueError("initial state is not set")
-
-        if self._current_state is None:
-            raise ValueError("state machine has not been started")
-
-        if propagate and self._current_state.has_child_sm():
-            logging.debug(f"Propagating evt {evt} from {self} to "
-                          f"{self._current_state.child_sm}")
-            self._current_state.child_sm.trigger_event(evt, data, propagate)
-        else:
-            for transition in self._transitions:
-                if transition.source_state == self._current_state and \
-                        transition.event == evt:
-                    self._current_state = transition.destination_state
-                    transition(data)
-                    if isinstance(self._current_state, ExitState) and \
-                            self._exit_callback and not self._exited:
-                        self._exited = True
-                        self._exit_callback(self._current_state, data)
-                    transition_valid = True
-                    break
-            if not transition_valid:
-                logging.warning(f"Event {evt} is not valid in state "
-                                f"{self._current_state}")
-
-    # @property
-    # def exit_state(self):
-    #     return self._exit_state
-
-
-'''
-
-
-
 from simpy import Process, Interrupt
 from simpy.events import PENDING, Initialize, Interruption
 from core import Environment
 import types
 from stores import Store
+from collections import OrderedDict
 
-def Generator(instance):
+
+def function(instance):
     def decorator(f):
         f = types.MethodType(f, instance)
-        setattr(instance, '_generator_function', f)
+        setattr(instance, '_function', f)
         return f
     return decorator
 
@@ -378,6 +78,7 @@ class StateMachine(object):
             setattr(self, state._name, state)
         self.copy_states()
         self.start()
+        self.env.add_object(self)
     def start(self):
         for state in self._states:
             if state.initial_state == True:
@@ -450,7 +151,7 @@ class State(Process):
         self._interrupt_callbacks = []
         self.env = None
         self._generator = None
-        self._generator_function = None
+        self._function = None
         self.initial_state = initial_state
         self.callbacks = []
     def __repr__(self):
@@ -487,7 +188,7 @@ class State(Process):
     def _do_start(self):
         self.callbacks = []
         self._value = PENDING
-        self._generator = self._generator_function
+        self._generator = self._function
         self._target = Initialize(self.env, self)
     def interrupt(self):
         if self.is_alive:
@@ -502,21 +203,21 @@ class State(Process):
                 self._child_state_machine.stop()
         else:
             print('Warning - interrupted state was not active')
-    def safe_generator(self,generator):
-        try:
-            yield from generator
-        except Interrupt:
-            for event in self.env._queue: 
-                if event[-1] == self._target:
-                    self.env._queue.remove(event)
-            for callback in self._interrupt_callback:
-                callback()
+    # def safe_generator(self,generator):
+    #     try:
+    #         yield from generator
+    #     except Interrupt:
+    #         for event in self.env._queue: 
+    #             if event[-1] == self._target:
+    #                 self.env._queue.remove(event)
+    #         for callback in self._interrupt_callback:
+    #             callback()
     def _resume(self, event):
         self.env._active_proc = self
         while True:
             try:
                 if isinstance(event,Initialize):
-                    event = self._generator_function()
+                    event = self._function()
                 elif event._ok:
                     try:
                         event = self._do(event)
@@ -574,11 +275,27 @@ class State(Process):
         self._target = event
         self.env._active_proc = None
 
+
+class CFSM(StateMachine):
+    def __init__(self,env,name):
+        super().__init__(env,name)
+        # self.message_ports = self.create_ports()
+        # for port in self.message_ports:
+        #     setattr(self, port._name, port)
+        temp=list(self.__dict__.keys())
+        self.message_ports()
+        self.messages = OrderedDict()
+        for i in list(self.__dict__.keys()):
+            if i not in temp:
+                self.messages[i] = getattr(self,i)
+    def message_ports(self):
+        pass
+        
                 
 class Boh(StateMachine):
     def build(self):
         Idle = State('Idle',True)
-        @Generator(Idle)
+        @function(Idle)
         def printt(self):
             print('%s is Idle' %self._parent_state_machine._name)
             return self.env.timeout(10)
@@ -593,7 +310,7 @@ class Boh(StateMachine):
             print('%s idle state interrupted ok'  %self._parent_state_machine._name)
         class Idle_SM(CompositeState):
             Sub = State('Sub',True)
-            @Generator(Sub)
+            @function(Sub)
             def printt(self):
                 print('%s will print this something in 20 s'  %self._parent_state_machine._name)
                 return self.env.timeout(20)
@@ -610,7 +327,7 @@ class Boh(StateMachine):
 class Boh2(StateMachine):
     def build(self):
         Work = State('Idle',True)
-        @Generator(Work)
+        @function(Work)
         def printt(self):
             print('Start working. Will finish in 10s')
             return self.env.timeout(10)
@@ -626,7 +343,7 @@ class Boh2(StateMachine):
             print('Entering working state')
         return [Work]
 # Idle = State('Idle')
-# @Generator(Idle)
+# @function(Idle)
 # def test(self):
 #     print('Test working')
 #     yield self.env.timeout(1)
@@ -649,16 +366,16 @@ class Boh2(StateMachine):
 
 # env.run(50)
 
-if False:
+if 1:
     env = Environment()
     foo = Boh2(env,1)
     env.run(20)
     foo.interrupt()
-    for i in range(10):
-        env.step()
+    # for i in range(10):
+    #     env.step()
     env.run(200)
     
-if False:
+if 1:
     env = Environment()
     foo = Boh(env,'Foo 1')
     foo2 = Boh(env,'Foo 2')
