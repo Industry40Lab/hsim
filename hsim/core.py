c6e64821ab58b0f49805aa2bce82d6dd32dd7c7e
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-05-23 12:27:41 +0200

    Update core.py

--- hsim/core.py
+++ hsim/core.py
@@ -39,3 +39,24 @@ def createLog():
     # global log
     log = pd.DataFrame(columns = ["entity","resource","operator","activity","timeIn","timeOut"])  
     return log
+
+class dotdict(dict):
+    """MATLAB-like dot.notation access to dictionary attributes"""
+    def __getattr__(self,name):
+        super().__getattr__(name)
+        return super().__getitem__(name)
+    def __setattr__(self,name,value):
+        super().__setitem__(name,value)
+        super().__setattr__(name, value)
+    def __delattr__(self,name):
+        super().__delattr__(name)
+        super().__delitem__(name)
+    def __repr__(self):
+        return str(vars(self))
+    def keys(self):
+        return vars(self).keys()
+    def values(self):
+        return vars(self).values()
+    def __len__(self):
+        return len(self.keys())
+    
