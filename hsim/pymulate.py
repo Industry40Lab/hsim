045465b7d0623b112e80c88a9ec9fc20879aeb4f
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-05-23 12:27:39 +0200

    Update pymulate.py

--- hsim/pymulate.py
+++ hsim/pymulate.py
@@ -3,19 +3,53 @@
 from chfsm import CHFSM, State, CompositeState
 from chfsm import function, do, on_entry, on_exit, on_interrupt
 from stores import Store
+from core import Environment
 
 class Server(CHFSM):
     def build(self):
-        Idle = State('Idle',True)
-        @function(Idle)
+        Starve = State('Starve',True)
+        @function(Starve)
         def sth(self):
-            return self.store.subscribe()
-        @do(Idle)
-        def u(self):
+            self.var.request = self.Store.subscribe()
+            return self.var.request
+        @do(Starve)
+        def u(self,event):
             return Work
         Work = State('Work')
-        return [Idle,Work]
-    def create_messages(self):
+        @function(Work)
+        def foo(self):
+            return self.env.timeout(10)
+        @do(Work)
+        def fcn(self,event):
+            a = self.connections['after'].put(1)
+            self.var.request.confirm()
+            # if self.
+        Block = State('Block')
+        @function(Block)
+        def block(self):
+            pass
+        @do(Block)
+        def blockk(self):
+            pass
+        return [Starve,Work,Block]
+    def build_c(self):
         self.Store = Store(self.env,1)
         
+class Generator(CHFSM):
+    def build(self):
+        self.connections['after'].put(entity)
+        Work = State('Work')
+        @function
+        def fcn(self):
+            return self.env.timeout(10)
+        @do
+        def do(self):
+            return self
 
+env = Environment()
+a1=CHFSM(env,1)
+a = Server(env,'1')
+a.Store.put([1])
+b = Store(env)
+a.connections['after']=b
+# env.run(15)
