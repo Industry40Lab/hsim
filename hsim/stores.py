dfddf99d7584074157c8e48890a4b9baee0aa67f
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-05-31 09:11:33 +0200

    Update stores.py

--- hsim/stores.py
+++ hsim/stores.py
@@ -4,9 +4,9 @@ Created on Sat Jan 15 14:54:49 2022
 
 @author: Lorenzo
 """
+from heapq import heappop
 
 from simpy import Event, FilterStore, Resource
-from heapq import heappop
 
 
 class Subscription(Event):
@@ -42,6 +42,13 @@ class Subscription(Event):
             self.resource._trigger_get(None)
     def read(self):
         return self.resource.items[0]
+    def check(self):
+        if self.item and len(self.resource.items) < self.resource._capacity:
+            return True
+        elif len(self.resource.items)>0:
+            return True
+        else:
+            return False
     def cancel(self):
         if not self.triggered:
             self.resource.put_queue.remove(self)
@@ -97,10 +104,94 @@ class Box(Store):
 
 
 class Resource(Resource):
-    pass
+    def __init__(self,env,capacity=1):
+        super().__init__(env,capacity)
+        self.items = self.users
+    def __len__(self):
+        return len(self.items)
+    def demand(self):
+        return Demand(self,None)
+    def forget(self,user=object()):
+        return Demand(self,user)
+    def _do_put(self, event):
+        if len(self.users) < self.capacity:
+            event.succeed()
+            if not isinstance(event,Demand):
+                self.users.append(event)
+                return 
+            else:
+                return True
+    def _do_get(self, event):
+        event.succeed()
+        if not isinstance(event,Subscription):
+            try:
+                self.users.remove(event.request)
+            except ValueError:
+                self.users.pop(0)
+            return
+        else:
+            event.succeed()
+            return True
+    def get_now(self,event):
+        try:
+            self.users.remove(event.request)
+        except ValueError:
+            self.users.pop(0)
+        return
+    def put_now(self,event):
+        self.users.append(event)
+        
+        # self._trigger_get(None)
+
+class Demand(Event):
+ # just like Get & Put events, but it does not get/put anything unless told to do so
+    def __init__(self, resource, user=None):
+        super().__init__(resource._env)
+        self.resource = resource
+        self.proc = self.env.active_process
+        self.user = user
+        if self.user is not None:
+            self.__append__(self,resource.get_queue)
+            resource._trigger_get(None)
+        else:
+            self.__append__(self,resource.put_queue)
+            resource._trigger_put(None)
+    def __append__(self,obj,container):
+        container.append(obj)
+    def renounce(self):
+        # to be triggered if refusing to get/put after subscribing
+        if self.user is not None:
+            self.resource._do_get()
+        else:
+            self.resource._do_put(self)
+    def confirm(self,user=None):
+        if self.user is not None:
+            self._value = self.resource.get_now()
+            self.resource._trigger_put(None)
+            return self.value
+        else:
+            if user:
+                self.user = user
+            self.resource.put_now(self.user)
+            self.resource._trigger_get(None)
+    # def read(self):
+    #     return self.resource.items[0]
+    def cancel(self):
+        if not self.triggered:
+            self.resource.put_queue.remove(self)
 
 
-
+        
+from core import Environment
+from simpy import AnyOf
+env = Environment()
+R = Resource(env)
+S = Store(env,1)
+S.put(1)
+r=R.demand()
+s=S.put(1)
+r=R.demand()
+a=AnyOf(env,[s,r])
 
 # class Subscription(Event):
 #     # just like Get & Put events, but it does not get/put anything unless told to do so
