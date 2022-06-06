5fd6d523803fdf426de6f02c17db4a9b1be01cb3
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-06-06 17:08:49 +0200

    Create utils.py

---
+++ hsim/utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 16:48:02 2022
+
+@author: Lorenzo
+"""
+
+   
+
+def stats(env):
+    y = env.state_log
+    t = env.now
+    y.loc[y.timeOut.values==None,'timeOut'] = t
+    stats = dict()
+    for res in y.Resource.unique():
+        v = dict()
+        for state in y.loc[y.Resource==res,'State'].unique():
+            x=y.loc[(y.Resource==res) & (y.State==state),('timeIn','timeOut')]
+            x = sum(x.timeOut - x.timeIn)/t
+            v[state._name] = x
+        stats[res] = v
+    return stats
