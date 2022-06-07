029d1e8b42db31a17ff356be5b2512de1c28fcc7
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-06-07 16:24:05 +0200

    Update MIPgame.py

--- hsim/MIPgame.py
+++ hsim/MIPgame.py
@@ -7,25 +7,33 @@ Created on Mon Jun  6 18:12:35 2022
 
 # MODEL
 
-from pymulate import Store, Environment, Generator, Server, ServerDoubleBuffer, Operator, ManualStation
-
-
-
+from pymulate import Store, Environment, Generator, Server, ServerDoubleBuffer, Operator, ManualStation, SwitchOut
 
 
 
 env = Environment()
+
 class gen_motor():
     def __init__(self):
         self.index = 0
     def __call__(self):
         self.index += 1
         return str('Motor %d' %self.index)
+    
 g_motor = Generator(env,'Motor Input',serviceTime=30,createEntity=gen_motor())
 motor1 = Server(env,'motor1',serviceTime=2)
+motor2a = ManualStation(env,serviceTime=2)
+motor2b = ManualStation(env,serviceTime=2)
+motor2c = ManualStation(env,serviceTime=2)
+s_out = SwitchOut(env)
+
+op1 = Operator(env, 'o1')
 T = Store(env)
 
 g_motor.connections['after'] = motor1
-motor1.connections['after'] = T
+op1.var.station = [motor2a,motor2b,motor2c]
+motor1.connections['after'] = s_out
+s_out.connections['after'] = [motor2a,motor2b,motor2c]
 # motor2.connections['after'] = motor3
-env.run(3600)
+s_out.put([1])
+env.run(1000)
