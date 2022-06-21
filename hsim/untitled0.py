f4dead0794e1a36ff881d4a78930dcc43365c537
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-06-21 08:54:39 +0200

    Update untitled0.py

--- hsim/untitled0.py
+++ hsim/untitled0.py
@@ -6,16 +6,20 @@ Created on Tue Jun 14 18:03:34 2022
 """
 
 import pymulate as sim
-from pymulate import Generator
+from pymulate import Generator, Server
 from pymulate import State
 from pymulate import function, do
+import numpy as np
+import pandas as pd
+
+
 
 
 class Generator(sim.Generator):
-    def __init__(self, env, name=None, createEntity=None,checkCondition=None):
+    def __init__(self, env, name=None, createEntity=None,releaseCheck=None):
         super().__init__(env, name, createEntity)
         self.var.Trigger = env.event()
-        self.var.checkCondition = checkCondition
+        self.var.releaseCheck = releaseCheck
     def build(self):
         Create = State('Create',True)
         @function(Create)
@@ -23,7 +27,7 @@ class Generator(sim.Generator):
             return self.var.Trigger
         @do(Create)
         def do2(self,event):
-            if self.var.checkCondition():
+            if self.var.releaseCheck():
                 try:
                     self.var.entity = self.var.createEntity()
                 except:
@@ -54,8 +58,28 @@ class CONWIP():
             return True
         else:
             return False
+            
+class createEntity():
+    def __init__(self,n_machines,config):
+        self.index = 1
+        self.n_machines = n_machines
+        self.config = config
+        self.n = 3
+    def __call__(self):
+        return Entity(self._serviceTime(),self._routing())
+    def _serviceTime(self):
+        pass
+    def _routing(self):
+        x = np.random.choice(range(1,1+self.n_machines),np.random.randint(1,1+self.n_machines),replace=False)
+        if self.config == 'F':
+            x.sort()
+        return [str('M%d' %i) for i in x]
 
-                
+class Entity():
+    def __init__(self,serviceTime,routing):
+        self.serviceTime = serviceTime
+        self.routing = routing
+   
             
 # %% code
 if __name__ == '__main__':
@@ -64,5 +88,9 @@ if __name__ == '__main__':
     T = sim.Store(env)
     g.connections['after'] = T
     g.var.Trigger.succeed()
-    g.var.checkCondition = CONWIP(5,[T])
+    g.var.releaseCheck = CONWIP(5,[T])
+    g.var.createEntity = createEntity(6,'F')
     env.run(10)
+    T.items.pop()
+    g.var.Trigger.succeed()
+    env.run(20)
