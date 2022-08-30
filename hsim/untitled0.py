ef29c8dbc4d009a9b48e342cd6752e81b6808a15
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2022-08-30 13:03:51 +0200

    Update untitled0.py

--- hsim/untitled0.py
+++ hsim/untitled0.py
@@ -6,13 +6,14 @@ Created on Tue Jun 14 18:03:34 2022
 """
 
 import pymulate as sim
-from pymulate import Generator, Server, Router, ServerDoubleBuffer,ServerWithBuffer, Queue, StoreSelect
+from pymulate import Generator, Server, Router0, ServerDoubleBuffer,ServerWithBuffer, Queue, StoreSelect
 from pymulate import function, action
 from chfsm import Transition, State
 import numpy as np
 import pandas as pd
 from simpy import AllOf, AnyOf
 from collections import OrderedDict
+from stores import Box, Store
 
         
 class Server(ServerWithBuffer):
@@ -21,14 +22,25 @@ class Server(ServerWithBuffer):
         self.Control = None
     def subscribe(self,item):
         return self.QueueIn.subscribe(item)
+    def put(self,item):
+        return self.QueueIn.put(item)
 Working = Server._states_dict('Working')
 Blocking = Server._states_dict('Blocking')
+Starving = Server._states_dict('Starving')
 @function(Working)
 def f(self):
     self.var.entity = self.var.request.read()
-    self.var.entity.routing.remove(self.sm._name)
+    if not self.sm._name[1] == 'x':
+        self.var.entity.routing.remove(self.sm._name)
 W2B = Transition(Working, Blocking, lambda self: self.env.timeout(self.calculateServiceTime(self.var.entity)),action=lambda self:self.Control.refresh())
 Working._transitions = [W2B]
+@function(Blocking)
+def fb(self):
+    target = self.sm.routing_target(self.sm.var.entity,self.sm.Next)
+    self.sm.var.requestOut = target.put(self.sm.var.entity)
+B2S = Transition(Blocking, Starving, lambda self: self.sm.var.requestOut,action=lambda self: self.sm.var.request.confirm())
+Blocking._transitions = [B2S]
+
 
 class PreShop(StoreSelect):
     def build(self):
@@ -37,27 +49,20 @@ class PreShop(StoreSelect):
 Sending = PreShop._states_dict('Sending')
 @function(Sending)
 def f20(self):
-    self.var.requestIn = self.Release
-    self.var.requestOut = []
-    self.var.requestDict = {}
-    for item in self.Queue.items:
-        if self.condition_check(item,self.Next):
-            self.var.requestOut.append(self.Next.subscribe(item))
-    if self.var.requestOut == []:
-        if len(self.Queue.items)==0:
-            self.Queue.put_event.restart()
-            self.var.requestIn = self.Queue.put_event
-        self.var.requestOut = [self.var.requestIn]
-refresh = Transition(Sending,Sending,lambda self:self.Release)
+    for item in self.sm.Queue.items:
+        if self.sm.condition_check(item,self.Next):
+            self.sm.Next.put(item)
+            self.sm.Queue.items.remove(item)
+    if len(self.sm.Queue.items)==0:
+        self.sm.Queue.put_event.restart()
+        self.sm.var.request = self.Queue.put_event
+    else:
+        self.sm.var.request = self.Release
+refresh = Transition(Sending,Sending,lambda self:self.var.request)
 @action(refresh)
 def f13(self):
-    if hasattr(self.var.requestOut[0],'item'):
-        for req in self.var.requestOut:
-            req.cancel()
     self.Release.restart()
-    
-Sending._transitions.pop(0)
-Sending._transitions.append(refresh)
+Sending._transitions = [refresh]
 
 class OR():
     def __init__(self,limits):
@@ -72,24 +77,32 @@ def calcWIP(obj):
             pass
     return x
 
-def countWIP(obj):
-    x = 0
-    for store in obj._messages.values():
-        try:
-            x += len(store.items)
-        except:
-            pass
-    return x
+# def countWIP(obj):
+#     x = 0
+#     for store in obj._messages.values():
+#         try:
+#             x += len(store.items)
+#         except:
+#             pass
+#     return x
 
 class CONWIP():
-    def __init__(self,limits):
+    def __init__(self,env,limits):
+        self.env = env
         self.limits = limits
         self.PreShop = None
-    def control(self,*args):
-        x = 0
+        self.list=self.listing(env)
+    def listing(self,env):
+        L = []
         for obj in env._objects:
             if type(obj) is not PreShop:
-                x += countWIP(obj)
+                for store in obj._messages.values():
+                    if str(type(store)) == str(Store):
+                        if hasattr(store,'items'):
+                            L.append(store)
+        return L
+    def control(self,*args):
+        x = sum([len(q.items) for q in self.list])
         if x<self.limits:
             return True
         else:
@@ -105,6 +118,7 @@ class CONWIP():
 class COBACABANA():
     def __call__(self,obj):
         pass
+
 
 class DEWIP():
     def __call__(self,item,station=None):
@@ -140,16 +154,11 @@ class Entity():
         self.serviceTime = serviceTime
         self.routing = routing
       
-def router_control(item,target):
+def router_control(item,target_list):
     if item.routing == []:
-        if target._name == 'T':
-            return True
-        else:
-            return False
-    elif item.routing[0] == target._name:
-        return True
+        return target_list[-1]
     else:
-        return False
+        return target_list[[i._name for i in target_list].index(item.routing[0])]
     
 
     
@@ -159,16 +168,14 @@ if __name__ == '__main__':
     np.random.seed(1)
     n_machines = 10
     env = sim.Environment()
-    C = CONWIP(12)
+    C = CONWIP(env,12)
     g = Generator(env,serviceTime=1)
-    init = Queue(env)
+    init = Server(env,serviceTime=0)
     preshop_pool = PreShop(env)
-    router = Router(env)
     servers = OrderedDict()
     for i in range(1,n_machines+1):
         name = 'M'+str(i)
-        servers[name] =Server(env,name,serviceTime=1)
-        servers[name].Next = router.Queue
+        servers[name] = Server(env,name,serviceTime=1)
         servers[name].Control = C
     T = sim.Store(env)
     T._name = 'T'
@@ -176,15 +183,18 @@ if __name__ == '__main__':
     g.Next = preshop_pool.Queue
     g.createEntity = createEntity(n_machines,'F')
     C.PreShop = preshop_pool
-    preshop_pool.Next = init.Store
-    init.Next = router.Queue
+    preshop_pool.Next = init
     preshop_pool.condition_check = C.control
-    router.Next = [server for server in servers.values()]+[T]
-    router.condition_check = router_control
+    init.Next = [server for server in servers.values()]+[T]
+    init.routing_target = router_control
+    init.Control = C
+    for server in servers.values():
+        server.Next = [server for server in servers.values()]+[T]
+        server.routing_target = router_control
     
     from time import time
     tic = time()
-    env.run(2000)
+    env.run(90)
     print(time()-tic)
     # 
     env.run(15)
