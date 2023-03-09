94ff18d7533d616a6ce4e67bed1c0dcdd8cc6679
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-03-09 09:46:07 +0100

    Update a.py

--- hsim/a.py
+++ hsim/a.py
@@ -1,66 +1,170 @@
 # -*- coding: utf-8 -*-
 
 import pymulate as pym
-from chfsm import Transition
-
+from chfsm import CHFSM, Transition, State
+import pandas as pd
+import numpy as np
+from simpy import AnyOf
 
 class Generator(pym.Generator):
     def createEntity(self):
-        pass
+        return object()
     
 class Entity:
     def __init__(self):
         self.ok = True
+        self.pt = dict()
+    @property
+    def require_robot(self):
+        if self.pt['M3']>0:
+            return True
+        else:
+            return False
         
-class Gate:
-    pass
+class Server(pym.Server):
+    def __init__(self,env,name=None,serviceTime=None,serviceTimeFunction=None):
+        self.trigger = env.event()
+        serviceTime = 10
+        super().__init__(env,name,serviceTime,serviceTimeFunction)
+    def completed(self):
+        if self.entity.ok:
+            self.trigger.succeed()
+            self.trigger.restart()
+    T2=Transition.copy(pym.Server.Working, pym.Server.Blocking, lambda self: self.env.timeout(self.calculateServiceTime(self.var.entity)), action = lambda self: self.completed())
 
-class GateCONWIP(Gate):
-    T2=Transition.copy(Generator.Creating,Generator.Sending,lambda self: self.env.timeout(self.calculateServiceTime(None)))
+class Terminator(pym.Terminator):
+    def __init__(self, env, capacity=np.inf):
+        super().__init__(env, capacity)
+        self.trigger = env.event()
+    def completed(self):
+        self.trigger.succeed()
+        self.trigger.restart()
+    def put(self,item):
+        super().put(item)
+        self.completed()
+        
+class Gate(CHFSM):
+    def __init__(self,env,DR,OR):
+        self.real = True
+        self.freq = 120
+        self.length = 300
+        self.capacity = 20
+        self.lab = None
+        self.DR = DR
+        self.OR = OR
+        self.monitored_DBR = list()
+        self.monitored_CONWIP = None
+        self.initialWIP = 12
+        super().__init__(env)
+    def build(self):
+        self.Store = pym.Store(self.env,self.capacity)
+    def put(self,item):
+        return self.Store.put(item)
+    def load(self):
+        print(self.initialWIP)
+        self.initialWIP = self.initialWIP - 1
+    class Waiting(State):
+        initial_state = True
+        def _do(self):
+            try:
+                e = self.Store.get()
+                self.Next.put(e.value)
+            except:
+                pass
+    class Controlling(State):
+        initial_state = True
+        def _do(self):
+            if self.real:
+                pass #run BN
+    T0 = Transition.copy(Waiting,Waiting,lambda self: self.env.timeout(1) ,lambda self: self.initialWIP>0, lambda self: self.load())
+    TA = Transition.copy(Waiting,Waiting,lambda self: AnyOf(self.env,self.monitored_DBR),lambda self: self.OR=='DBR')
+    TB = Transition.copy(Waiting,Waiting,lambda self: self.monitored_CONWIP,lambda self: self.OR=='CONWIP')
+    TC = Transition.copy(Controlling,Controlling,lambda self: self.env.timeout(self.freq))
 
-class GateDBR(Gate):
-    T2=Transition.copy(Generator.Creating,Generator.Sending,lambda self: self.env.timeout(self.calculateServiceTime(None)))
 
+class RobotSwitch1(pym.Router):
+    def condition_check(self, item, target):
+        if item.require_robot and target == "":
+            return True
+        elif not item.require_robot and target != "":
+            return True
+        else:
+            return False
+            
+class RobotSwitch2(pym.Router):
+    def condition_check(self, item, target):
+        if len(target)<2:
+            item.rework = False
+            return True
+        else:
+            item.rework = True
+            return False    
 
+class CloseOutSwitch(pym.Router):
+    def condition_check(self, item, target):
+        if item.ok and target == "":
+            return True
+        elif not item.ok and target != "":
+            return True
+        else:
+            return False
+        
+class Conveyor(pym.ParallelServer):
+    def __init__(self,env,name=None,serviceTime=None,serviceTimeFunction=None,capacity=1):
+        self._capacity = capacity
+        self.serviceTime = capacity*3.5
+        super().__init__(env,name,serviceTime,serviceTimeFunction)
 
 class Lab:
     def __init__(self,DR:str,OR:str,BN:str):
         self.env = pym.Environment()
         self.env.BN = None
         self.g = Generator(self.env)
-        if OR == 'CONWIP':
-            gate = GateCONWIP(self.env)
-        elif OR == 'CONWIP':
-            gate = GateDBR(self.env)
+        self.gate = Gate(self.env,DR,OR)
         
-        self.conv1 = pym.ParallelServer(self.env,capacity=3)
-        self.front = pym.Server(self.env)
-        self.conv2 = pym.ParallelServer(self.env,capacity=3)
-        self.drill = pym.Server(self.env)
-        self.conv3 = pym.ParallelServer(self.env,capacity=3)
-        self.switch = pym.Router(self.env)
-        self.convRobot = pym.ParallelServer(self.env,capacity=3)
-        self.bridge = pym.ParallelServer(self.env,capacity=3)
-        self.conv4 = pym.ParallelServer(self.env,capacity=3)
-        self.robot = pym.Server(self.env)
-        self.conv5 = pym.ParallelServer(self.env,capacity=3)
-        self.camera = pym.Server(self.env)
-        self.conv6 = pym.ParallelServer(self.env,capacity=3)
-        self.back = pym.Server(self.env)
-        self.conv7 = pym.ParallelServer(self.env,capacity=3)
-        self.press = pym.Server(self.env)
-        self.conv8 = pym.ParallelServer(self.env,capacity=10)
-        self.manual = pym.Server(self.env)
+        self.conv1 = Conveyor(self.env,capacity=3)
+        self.front = Server(self.env)
+        self.conv2 = Conveyor(self.env,capacity=3)
+        self.drill = Server(self.env)
+        self.conv3 = Conveyor(self.env,capacity=3)
+        
+        self.switch1 = RobotSwitch1(self.env)
+        self.convRobot1 = Conveyor(self.env,capacity=3)
+        self.bridge = Conveyor(self.env,capacity=3)
+        self.convRobot2 = Conveyor(self.env,capacity=3)
+        self.switch2 = RobotSwitch2(self.env)
+        self.convRobot2 = Conveyor(self.env,capacity=3)
+        self.convRobot3 = Conveyor(self.env,capacity=3)
+        self.robot = Server(self.env)
+        self.convRobotOut = Conveyor(self.env,capacity=3)
+        self.conv5 = Conveyor(self.env,capacity=3)
+        self.camera = Server(self.env)
+        self.conv6 = Conveyor(self.env,capacity=3)
+        self.back = Server(self.env)
+        self.conv7 = Conveyor(self.env,capacity=3)
+        self.press = Server(self.env)
+        self.conv8 = Conveyor(self.env,capacity=10)
+        self.manual = Server(self.env)
+        self.outSwitch = CloseOutSwitch(self.env)
+        self.terminator = Terminator(self.env)
+        
+        self.g.Next = self.gate
+        self.gate.Next = self.conv1
         
         self.conv1.Next = self.front
         self.front.Next = self.conv2
         self.conv2.Next = self.drill
         self.drill.Next = self.conv3
-        self.conv3.Next = self.switch
-        self.switch.Next = [self.convRobot,self.bridge]
-        self.convRobot.Next = self.robot
+        self.conv3.Next = self.switch1
+        
+        self.switch1.Next = [self.convRobot1,self.bridge]
+        self.convRobot1.Next = self.switch2
+        self.switch2.Next = [self.convRobot2,self.convRobot3]
+        self.convRobot3.Next = self.convRobotOut
+        self.robot.Next = self.convRobotOut
+        self.convRobotOut.Next = self.conv5
         self.bridge.Next = self.conv5
-        self.robot.Next = self.conv5
+        
         self.conv5.Next = self.camera
         self.camera.Next = self.conv6
         self.conv6.Next = self.back
@@ -68,12 +172,18 @@ class Lab:
         self.conv7.Next = self.press
         self.press.Next = self.conv8
         self.conv8.Next = self.manual
-        self.manual.Next = self.conv1
-
-
-
-
+        self.manual.Next = self.outSwitch
+        self.outSwitch.Next = [self.conv1,self.terminator]
+        
+        self.gate.monitored_DBR = [x.trigger for x in [self.front,self.drill,self.robot,self.camera,self.back,self.press,self.manual]]
+        self.gate.monitored_CONWIP = self.terminator.trigger
 
     def run(self,Tend):
         self.env.run(Tend)
+        return pd.DataFrame(self.env.state_log)
 
+
+lab=Lab('FIFO','CONWIP','PAST')
+for i in range(20):
+    lab.gate.put(object())
+lab.run(1000)
