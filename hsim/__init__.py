34f7a629b016f28a8ff9a26c7d64258451a68437
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2023-11-10 14:04:17 +0100

    Update __init__.py

--- hsim/__init__.py
+++ hsim/__init__.py
@@ -1,5 +1,8 @@
-# from .resources import resources
-from .core import core
-from .states import states
-from .stores import stores
+#from .resources import resources
+#from .core import core
+#from .states import states
+#from .stores import stores
 from hfsm import hfsm
+import .core
+import .states
+import .stores
