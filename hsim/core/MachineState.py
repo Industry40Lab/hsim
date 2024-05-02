f88e123b47a7566d568b21dbf1bb5f678479dc5a
Lorenzo <lorenzo.ragazzini@outlook.com>
2024-05-02 14:34:57 +0200

    update Untitled-1.ipynb and MachineState.py

--- hsim/core/MachineState.py
+++ hsim/core/MachineState.py
@@ -8,10 +8,17 @@ class MachineState(Component,State):
         State.__init__(self, name, trigger, **kwargs)
         Component.__init__(self, name)
         self._transitions:Iterable["Transitions"] = list() # type: ignore
-    def _do(self):
+    def _do(self)->None:
         pass
     def process(self):
-        return self._do()
+        self._do()
+        return
+        triggers = [t._trigger for t in self._transitions]
+        self.wait(triggers)
+        # get trigger triggered, in order
+        # evaluate condition
+        # perform action
+        # return new_state
     def set_parent_sm(self, sm:"StateMachine") -> None: # type: ignore
         self.sm = sm
     def __call__(self) -> None:
