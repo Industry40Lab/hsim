9de5fe4cb1695a4b4fb5db9b42344cd47e4e3b44
lorenzo-ragazzini <80841175+lorenzo-ragazzini@users.noreply.github.com>
2024-10-16 23:49:42 +0200

    update transitions.py

---
+++ hsim/new/core/resources.py
@@ -0,0 +1,22 @@
+
+from pymulate import Server, Generator, Terminator
+from FSM import FSM
+from states import State
+from transitions import MessageTransition, TimeoutTransition, EventTransition
+
+class UnreliableMachine(Server):
+    class FSM(Server.FSM):
+        class Failed(State):
+            pass
+        
+        S2W=MessageTransition.define(FSM.Starving, FSM.Working)
+        S2F=MessageTransition.define(FSM.Starving, Failed)
+        F2B=TimeoutTransition.define(Failed, FSM.Working)
+        W2B=TimeoutTransition.define(FSM.Working, FSM.Blocking)
+        B2S=EventTransition.define(FSM.Blocking, FSM.Starving)
+        
+        
+# needing pseudostate or multiple transitions=??
+class Pseudostate():
+    def start(self):
+        pass
