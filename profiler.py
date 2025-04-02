51e93b2191c8bd919ec7b89ed8c1fc0642e62ce8
lorenzo-ragazzini <lorenzo.ragazzini@outlook.com>
2025-04-02 15:46:02 +0200

    Code profiles

---
+++ profiler.py
@@ -0,0 +1,31 @@
+import cProfile
+import pstats
+from hsim.GSOM.GSOMGame import main
+import pandas as pd
+
+
+
+
+profiler = cProfile.Profile()
+try:
+    profiler.runcall(main, "", app=False, pa=True)
+except Exception as e:
+    print(f"Error: {e}")
+# Save profiler stats to an Excel file using pandas
+
+stats = pstats.Stats(profiler)
+stats.sort_stats("cumtime")
+data = [
+    {
+        "Function": f"{func[0]}:{func[1]}({func[2]})",
+        "Calls": cc,
+        "Total Time": tt,
+        "Cumulative Time": ct,
+        "Per Call (Total)": tt / nc if nc else 0,
+        "Per Call (Cumulative)": ct / nc if nc else 0,
+    }
+    for func, (cc, nc, tt, ct, callers) in stats.stats.items()
+]
+df = pd.DataFrame(data).to_excel("profiler_stats.xlsx", index=False)
+
+
