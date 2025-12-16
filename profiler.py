1ab072a6dee8944b0f7b7ea0f3596ef4a158313b
Lorenzo <>
2025-12-16 15:08:12 +0100

    Cleanup

--- profiler.py
+++ profiler.py
@@ -4,14 +4,21 @@ from hsim.GSOM.GSOMGame import main
 import pandas as pd
 
 
-filename = r"C:\Users\Lorenzo\DIG Dropbox\Lorenzo Ragazzini\Didattica\MIP-GSOM Game\20230323 GSOM MBA 2023 INDUSTRY40 SIMULATION CASE\GSOM_original.xlsx"
+filename = r"data/profiler/GSOM_original.xlsx"
+
+if 1:
+    from time import time
+    t = time()
+    main(filename=filename, app=True)
+    print(f"Execution time without profiling: {time()-t} seconds")
+    raise SystemExit()
 
 profiler = cProfile.Profile()
 try:
     profiler.runcall(main, filename=filename, app=True)
 except Exception as e:
     print(f"Error: {e}")
-profiler.dump_stats("output.prof")
+profiler.dump_stats("data/profiler/output.prof")
 
 stats = pstats.Stats(profiler)
 stats.sort_stats("cumtime")
@@ -22,10 +29,10 @@ data = [
         "Total Time": tt,
         "Cumulative Time": ct,
         "Per Call (Total)": tt / nc if nc else 0,
-        "Per Call (Cumulative)": ct / nc if nc else 0,
+        "Per Call (Cumulative)": ct / nc if nc else 0
     }
     for func, (cc, nc, tt, ct, callers) in stats.stats.items()
 ]
-df = pd.DataFrame(data).to_excel("profiler_stats.xlsx", index=False)
+df = pd.DataFrame(data).to_excel("data/profiler/profiler_stats.xlsx", index=False)
 
 
